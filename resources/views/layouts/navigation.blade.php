8194be549e23c3a28fe827c0b24cc0a3948e1a03
Gayan Kavinda <gayan982822513.lk@gmail.com>
2024-10-28 11:30:52 +0530

    color changed user name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,17 +16,18 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="flex items-center px-4 py-2 bg-gray-700 text-gray-300 rounded-md hover:bg-gray-600 hover:text-white transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                    </x-slot>
+<!-- Settings Dropdown -->
+<div class="hidden sm:flex sm:items-center">
+    <x-dropdown align="right" width="48">
+        <x-slot name="trigger">
+            <button class="flex items-center px-4 py-2 bg-gray-700 text-gray-200 font-semibold rounded-md hover:bg-gray-600 hover:text-white transition ease-in-out duration-150">
+                <div class="mr-2">{{ Auth::user()->name }}</div>
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </x-slot>
+
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')" class="text-gray-700 hover:bg-gray-100">
